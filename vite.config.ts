061aeb3c50acecb143681f93937d78ddb75aeab6
vamshikrishna <vamshi@bizionictech.com>
2025-02-28 15:22:41 +0530

    meta discriotion

--- vite.config.ts
+++ vite.config.ts
@@ -6,12 +6,8 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: '0.0.0.0',
+    host: "::",
     port: 8080,
-    strictPort: true, 
-    allowedHosts: [
-       '4c51-2405-201-c008-4091-f049-6e84-c34f-4f7.ngrok-free.app'
-    ]
   },
   plugins: [
     react(),
